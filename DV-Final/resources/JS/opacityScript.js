56602ac06019650ba830c1aa7be7f52a2354fd0f
victorleezhesheng <36871691+victorleezhesheng@users.noreply.github.com>
2020-11-27 11:41:30 +0800

    Opacity

--- DV-Final/resources/JS/opacityScript.js
+++ DV-Final/resources/JS/opacityScript.js
@@ -1,5 +1,20 @@
 function init() {
   // Set constant
+  const bg = document.getElementById("bg");
+  const mountain = document.getElementById("mountain");
+  const girl = document.getElementById("girl");
+  const text = document.getElementById("text");
+
+  $(document).ready(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 0) {
+        $(".topbar").css({ opacity: "0" });
+      } else {
+        $(".topbar").css({ opacity: "1" });
+      }
+    });
+  });
+
   // opacity decrease function
 }
 window.onload = init;
