88b3b2a89fd87f947c332a706bb423d2c13a6d66
victorleezhesheng <36871691+victorleezhesheng@users.noreply.github.com>
2020-11-28 16:01:26 +0800

    Marine

--- DV-Final/resources/JS/topScript.js
+++ DV-Final/resources/JS/topScript.js
@@ -3,9 +3,16 @@ function init() {
   const mountain = document.getElementById("mountain");
   const girl = document.getElementById("girl");
   const text = document.getElementById("text");
+  const topbar = document.getElementById("topbar");
 
   window.addEventListener("scroll", () => {
     const value = window.scrollY;
+
+    if (value > 0) {
+      topbar.style.opacity = 0;
+    } else {
+      topbar.style.opacity = 1;
+    }
 
     bg.style.top = value * 0.5 + "px";
     mountain.style.top = -value * 0.08 + "px";
